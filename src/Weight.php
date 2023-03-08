2bd6f921adac5ee9439b1ae60635b3f175006973
Sucre Xu <sucrexu@outlook.com>
2023-03-08 16:19:12 +0800

    Test auto style fix provided by github action.

--- src/Weight.php
+++ src/Weight.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace Kisssg\UnitConversions;
+use Exception;
 
 class Weight
 {
     private float $kilograms;
 
-    public static function fromKilograms(float $kilograms): self
-    {
+    public static function fromKilograms(float $kilograms): self {
+        $myArr = [
+            'test',
+            'test2',
+        ];
+
         return new static($kilograms);
     }
 
